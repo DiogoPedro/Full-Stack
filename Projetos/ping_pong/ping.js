0d18e10bad2e3735b50a167915c13f0f95812b3f
DiogoPedro <dpfs@cin.ufpe.br>
2022-02-09 09:42:45 -0300

    "Estrutura Pronta"

--- Projetos/ping_pong/ping.js
+++ Projetos/ping_pong/ping.js
@@ -1,30 +1,26 @@
 window.onload = init;
 
 function init() {
-    //testePosition();
-    //findPlayer1();
-    keyPressionado();
+    //Elements in game;
+    let btnStart = document.getElementById("initButton");
+    let player1 = document.getElementById("player1");
+    let player2 = document.getElementById("player2");
+    let square = document.getElementById("square");
+
+    //Position in the game;
+    let xPlayer1, yPlayer1;
+    let xPlayer2, yPlater2;
+    let xSquare, ySquare;
+
+    //Direction
+    let game, frames;
+
+    //Position Inital
+    let [xInitPlayer1, yInitPlayer1] = [350, 325];
+    let [xInitPlayer2, yInitPlayer2] = [1170, 325];
+    let [xInitSquare, yInitSquare] = [442,220];
+
+    //Data in the game
+    let [playerHeight, playerWidth] = [20, 85];
+    let [mapHeight, mapWidth, squareSide] = [500, 900];
 };
-function findPlayer1(){
-    let playerOne = document.getElementById("player1");
-    let rect = playerOne.getBoundingClientRect();
-    alert(rect.left);
-
-    playerOne.style.left = (rect.left + 0.20) + "px";
-
-    rect = playerOne.getBoundingClientRect();
-    alert(rect.left);
-}
-
-function testePosition(){
-    let myLayer = document.getElementById("player1");
-    let x = myLayer.style.left;
-    alert(x);
-}
-
-function keyPressionado(){
-    let mapGame = document.getElementById("mapId");
-    mapGame.addEventListener('keypress', () => {
-    alert("Apertou o botao");
-    });
-}
