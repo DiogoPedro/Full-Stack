68293d6cb4c5c1879c6fe57039e16fb330fd3498
DiogoPedro <dpfs@cin.ufpe.br>
2022-02-10 17:50:36 -0300

    "Novas atualizacoes com o pingpong e novos conhecimentos com o uso do json"

--- Projetos/ping_pong/ping.js
+++ Projetos/ping_pong/ping.js
@@ -3,24 +3,52 @@ window.onload = init;
 function init() {
     //Elements in game;
     let btnStart = document.getElementById("initButton");
-    let player1 = document.getElementById("player1");
-    let player2 = document.getElementById("player2");
-    let square = document.getElementById("square");
-
-    //Position in the game;
+    let elPlayer1 = document.getElementById("player1");
+    let elPlayer2 = document.getElementById("player2");
+    let elSquare = document.getElementById("square");
+    
+    //Position in the game in coordinates;
     let xPlayer1, yPlayer1;
     let xPlayer2, yPlater2;
     let xSquare, ySquare;
-
-    //Direction
-    let game, frames;
-
-    //Position Inital
-    let [xInitPlayer1, yInitPlayer1] = [350, 325];
-    let [xInitPlayer2, yInitPlayer2] = [1170, 325];
-    let [xInitSquare, yInitSquare] = [442,220];
-
+    
+    //Direction and control
+    let game, frames, control = false;
+    
     //Data in the game
     let [playerHeight, playerWidth] = [20, 85];
     let [mapHeight, mapWidth, squareSide] = [500, 900];
+
+    //Events about the game;
+    btnStart.addEventListener("click", startGame);
+    document.addEventListener("keydown", movement);
+    document.addEventListener("keyup",movement2);
+    
+    function startGame(){
+        if(!control){
+            //Break loop in the requestAnimationFrame;
+            cancelAnimationFrame(frames);
+
+            //Position Inital [x,y];
+            [xPlayer1, yPlayer1] = [350, 310];
+            [xPlayer2, yPlater2] = [1170, 310];
+            [xSquare, ySquare] = [442,220];
+
+            //Game initialized;
+            control = true;
+            game();
+        }
+    };
+    function movement(){
+        alert(event.keyCode);
+    };
+    function movement2(){
+        let keyPress2 = event.keyCode;
+        console.log(keyPress2);
+    };
+    function game(){
+        if(control){
+        }
+        frames = requestAnimationFrame(game);
+    };
 };
