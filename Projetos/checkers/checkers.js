4dee17110b25b562133cd8f9bbc29c79c0bef50d
DiogoPedro <dpfs@cin.ufpe.br>
2022-02-19 19:02:43 -0300

    "Atualizando meu repositorio: Consertando bug do ping e adicionando novas funcionalidades para checkers"

--- Projetos/checkers/checkers.js
+++ Projetos/checkers/checkers.js
@@ -2,35 +2,45 @@ window.onload = init;
 
 function init() {
     criarTabuleiro();
+    //criarPecas();
 };
-
-function moviment(evt) {
-    alert(evt.currentTarget.innerText);
-};
-
 function criarTabuleiro() {
     let elGame = document.getElementById("game");
     let positionsChars = ["A", "B", "C", "D", "E", "F", "G", "H"];
 
     for (let i = 1; i <= 100; i++) {
-        let create = criarElement();
+        let create = document.createElement("p");
+        //Adicionando no tabuleiro a primeira e ultima coluna com os valores entre 1 a 8;
         if ((i > 1 && i < 10) || (i > 91 && i < 100)) {
             if (i != 1) {
                 create.innerText = positionsChars[i % 10 - 2];
+                create.style.backgroundColor = "rgb(211, 211, 211)";
             };
         };
-
+        //Adicionando no tabuleiro a primeira e ultima linha com positionsChars;
         if (i > 10 && i <= 90) {
             if (i % 10 == 1 || i % 10 == 0) {
                 create.innerText = Math.floor((i - 1) / 10);
+                create.style.backgroundColor = "rgb(211, 211, 211)";
             }
         }
-        create.addEventListener("click", moviment);
+        //Preenchimento das bordas;
+        if (i == 1 || i == 10 || i == 91 || i == 100){
+            create.style.backgroundColor = "rgb(211, 211, 211)";
+        }
+        //Adicionando o evento nos grid itens do tabuleiro jogavel;
+        if (i > 11 && i < 90 && i % 10 > 1){
+            create.addEventListener("click", moviment);
+        }
         elGame.appendChild(create);
     }
 }
-
-function criarElement() {
-    let x = document.createElement("p");
-    return x;
+// function criarPecas(){
+//     let elGame = document.getElementById("game");
+//     for(i=13; i<41; i+=2){
+        
+//     }
+// };
+function moviment(evt) {
+    alert(evt.currentTarget.innerText);
 };
